a9a20c6c239fb4fd6ecc9ec1e2279efdb09673c1
AAnnuj <140152831+AAnnuj@users.noreply.github.com>
2023-07-21 13:50:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// const cards = document.querySelectorAll('.card')
+
+// cards.forEach(card => {
+//     card.addEventListener('click', () => {
+//         removeActiveClasses()
+//         panel.classList.add('active')
+//     })
+// })
+
+// function removeActiveClasses() {
+//     panels.forEach(panel => {
+//         panel.classList.remove('active')
+//     })
+// }
